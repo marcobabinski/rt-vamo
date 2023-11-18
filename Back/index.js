04d045e57f2bd4f771222fb78cc517df7e52cd2a
Alisson <alisson.rosa@digifred.com.br>
2023-11-18 16:45:49 -0300

    feat: add new endpoint to genre

--- Back/index.js
+++ Back/index.js
@@ -7,7 +7,7 @@ let id;
     response.send("Primary page")
   })
 
-  // Get all products
+  // Get all films
   app.get('/films', async (request, response) => {
     try {
         const result = await db.query('SELECT * FROM films');
@@ -20,7 +20,6 @@ let id;
 
   app.get('/films/:id', async (request, response) => {
     try {
-        console.log(`the id is: ${request.params.id}`);
         const result = await db.query(`SELECT * FROM films where i_film=${request.params.id}`);
         response.json(result.rows);
     } catch (error) {
@@ -31,17 +30,26 @@ let id;
 
   app.get('/genre/:genre', async (request, response) => {
     try {
-        console.log(`the id is: ${request.params.genre}`);
         const genre = await db.query(`SELECT i_genre FROM genres where name='${request.params.genre}'`);
         const films = await db.query(`SELECT * FROM films where i_film=${genre.rows[0].i_genre}`)
-        console.log('result', films.rows);
 
-        // response.json(result.rows);
+        response.json(films.rows);  
     } catch (error) {
         console.error(error);                                                     
         response.status(500).send('Internal Server Error'); 
     }
   });
+
+  app.get('/genre', async (request, response) => {
+    try {
+      const result = await db.query(`SELECT * FROM genres`);
+
+      response.json(result.rows);
+    } catch (error) {
+      console.error(error);                                                     
+      response.status(500).send('Internal Server Error'); 
+    }
+  })
 
   app.get('/reviews', async (request, response) => {
     try {
@@ -63,15 +71,6 @@ let id;
         response.status(500).send('Internal Server Error'); 
     }
   });
-
-  // app.get(`api/product/id`), async (request, response) => {
-  //   try {
-  //     const result = await db.query('SELECT * FROM produto');
-  //     response.json(result.rows);
-  //   } catch (error){
-  //     console.error(error)
-  //   }
-  // }
 
   const PORT = 8000
   app.listen(PORT, () => {
